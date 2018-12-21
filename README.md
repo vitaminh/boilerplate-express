1cfe13e541f6e78e4c57faa6b6f84330528dab60
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-12-20 19:36:10 -0800

    Complete CSS Setup

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # boilerplate-express
-Boilerplate Code for a Web App using express.js. Provides a starting point for a fullstack web application using: 
+A starting point for a fullstack web application using: 
 
 * React
 * Redux
-* Express
+* Express.js
 * Sequelize
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 module.exports = {
   entry: './client/index.js',
-  mode: 'development',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
-  devtool: 'source-maps',
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
